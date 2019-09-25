dac5b09d6b5b192c95b415f4561e9fe3f9662b69
martin-linden <martin.linden@student.kyh.se>
2019-09-25 15:52:32 +0200

    mindre andringar

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var toDoItems = [];
 
 function getValue() {
-	//hämtar värdet från textrutan och sparar i variabeln - theItem
+	/// retrieves the value from the text box and saves in the variable - theItem
 	var theItem = document.getElementById('enter').value;
-
-	//stoppas om stängen är tom
+	// creating checkbox element
+	//Is stopped if the string is empty
 	if (theItem == '' || theItem.length == 0) {
 		return false;
 	}
-	//pushar in theItem i våran array
+
+	//push theItem into the array
 	toDoItems.push(theItem);
-	alert(toDoItems);
-	//visar children av vår <div> i html?? osäker på hur den här delen fungerar exakt.
+	//display children of <ul> id = 'list'
 	console.log(document.getElementById('list').children);
 	document.getElementById('list').innerHTML +=
 		'<li>' + toDoItems[toDoItems.length - 1] + '</li>';
+
+	/*var list = document.getElementById('list');
+	var checkbox = document.createElement('input');*/
 
 	//Clears input text
 	document.getElementById('enter').value = '';
@@ -26,113 +29,20 @@ function deletebutton() {
 	console.log('deletebutton ran!');
 	toDoItems = [];
 	list.innerHTML = '';
-	/* while (list.firstChild) {
-        list.removeChild(list.firstChild);
-    } */
 }
 
-/*
+function kryssruta() {
+	var checkBox = document.getElementById('myCheck');
+	var checkmatt = document.getElementById('XXX');
 
-var toDoItems = [];
-
-function getValue() {
-    //hämtar värdet från textrutan och sparar i variabeln - theItem
-    var theItem = document.getElementById('enter').value;
-}
-//stoppas om stängen är tom
-if (theItem == '' || theItem.length == 0) {
-    return false;
-}
-//pushar in theItem i våran array
-toDoItems.push(theItem);
-//visar children av vår <div> i html?? osäker på hur den här delen fungerar exakt.
-document.getElementById('list').children[0].innerHTML +=
-    '<li>' + toDoItems[toDoItems.length - 1] + '</li>';
-
-//Clears input text
-document.getElementById('enter').value = '';
-// Focuses on input field
-document.getElementById('enter').focus();
-//deletebutton, does'nt loop. But it removes everything in the list.
-function deletebutton() {
-    console.log('deletebutton ran!');
-    //toDoItems = [];
-    while (toDoItems > -1) {
-        toDoItems.pop;
-    }
+	if (checkBox.checked == true) {
+		text.style.display = 'block';
+	} else {
+		text.style.display = 'none';
+	}
 }
 
-// function updateDom() {
-//  while(ul.firstChild) {
-//      ul.removeChild(ul.firstChild)
-//  }
-
-//DEN ANDRA KODEN
-
-// //Add items to our list
-// toDoItems = [];
-// let name = '';
-
-// function addItem(x) {
-//  var toDo = [ x, (bought = false) ];
-//  toDoItems.push(toDo);
-
-//  alert('The list contains ' + toDoItems);
-//  clearInterval(toDo);
-// }
-
-// function getValue() {
-//  var y = document.getElementById('item');
-//  var x = document.getElementById('form').value;
-//  y.innerHTML = x;
-//  addItem(x);
-// }
-
-// getValue();
-
-/*Toms kod:
-        var myArray = ["hej", "hej2", "hej3"];
-
-        const ul = document.querySelector("ul");
-        //ul.remove();
-
-        const form = document.querySelector("form");
-
-        const input = document.querySelector("input");
-
-        function updateDom() {
-            while(ul.firstChild) {
-                ul.removeChild(ul.firstChild)
-            }
-            for(let i = 0; i < myArray.length; i += 1){
-                const li = document.createElement("li");
-                li.textContent = myArray[i];
-                li.classList.add("todo-item")
-                ul.appendChild(li);
-            }
-        }
-
-        updateDom();
-
-        form.addEventListener("submit", (e) =>{
-            e.preventDefault();
-            const newTodo = input.value;
-            myArray.unshift(newTodo);
-            input.value = "";
-            updateDom();
-        });
-
-        const items = document.querySelectorAll("li");
-
-        ul.addEventListener("click", e =>{
-                console.log("item clicked");
-                //console.log(e);
-                //console.log(e.target);
-                //console.log(item);
-                //e.target.style.textDecoration = "line-through";
-                if(e.target.classList.contains("todo-item")) {
-                    e.target.remove();
-                }
-
-        });
-        */
+checkbox.type = 'DWADWAD';
+checkbox.value = 'ndiwojdo';
+checkbox.name = 'dwadaw';
+checkbox.id = 'hduiwh';
