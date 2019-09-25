4dec41880ed8f4f7810893336ba608ad5327e268
martin-linden <martin.linden@student.kyh.se>
2019-09-25 09:58:18 +0200

    commit 2, ny js, nya funktioner, ny html

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-//Add items to our list
-toDoItems = [];
-let name = '';
-
-function addItem(x) {
-	var toDo = [ x, (bought = false) ];
-	toDoItems.push(toDo);
-
-	alert('The list contains ' + toDoItems);
-	clearInterval(toDo);
-}
+var toDoItems = [];
 
 function getValue() {
-	var y = document.getElementById('item');
-	var x = document.getElementById('form').value;
-	y.innerHTML = x;
-	addItem(x);
+	//hämtar värdet från textrutan och sparar i variabeln - theItem
+	var theItem = document.getElementById('enter').value;
+
+	//stoppas om stängen är tom
+	if (theItem == '' || theItem.length == 0) {
+		return false;
+	}
+
+	{
+		//pushar in theItem i våran array
+		toDoItems.push(theItem);
+		//visar children av vår <div> i html?? osäker på hur den här delen fungerar exakt.
+		document.getElementById('list').children[0].innerHTML +=
+			'<li>' + toDoItems[toDoItems.length - 1] + '</li>';
+	}
+
+	// 	 Clears input text
+	//     document.getElementById('form').value = ''
+	//     // Focuses on input field
+	//     document.getElementById('form').focus();
+	// ska ligga i funktionen getValue
+
+	// function deletebutton() {
+	// 	console.log(“deletebutton ran!“)
+	// 	toDoItems = [];
+	//  }
+	//  <div>
+	// 		<button id=“deletebutton” onclick=“deletebutton()“> Radera! </button>
+	// 	</div>
+
+	//DEN ANDRA KODEN
+
+	// //Add items to our list
+	// toDoItems = [];
+	// let name = '';
+
+	// function addItem(x) {
+	// 	var toDo = [ x, (bought = false) ];
+	// 	toDoItems.push(toDo);
+
+	// 	alert('The list contains ' + toDoItems);
+	// 	clearInterval(toDo);
+	// }
+
+	// function getValue() {
+	// 	var y = document.getElementById('item');
+	// 	var x = document.getElementById('form').value;
+	// 	y.innerHTML = x;
+	// 	addItem(x);
+	// }
+
+	// getValue();
 }
-
-getValue();
-
-//Confirm to do
-
-// Checkbox
-
-//Delete from list
-
-//Delete all items from the list
-
-/*
-function removeAll() {
-    document.getElementById('XXX').innerHTML = '';
-}
-*/
-
-//List items//Delete all items from the list
-
-/*
-function removeAll() {
-    document.getElementById('XXX').innerHTML = '';
-}
-*/
-
-//List items
